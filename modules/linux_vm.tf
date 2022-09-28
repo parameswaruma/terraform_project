5908aa6ed74352cb266236f95cdcc68de26bf849
umaparameswar <parameswarkollati@gmail>
2022-09-28 10:16:46 +0530

    changes region to east us

--- modules/linux_vm.tf
+++ modules/linux_vm.tf
@@ -1,7 +1,7 @@
 module "linuxvm" {
     source = "../terraform/linuxvm"
     resource_group_name="minirg"
-    resource_group_location="Central US"
+    resource_group_location="East US"
     virtual_network_name="vnet1"
     subnet_name="subnet1"
     network_security_group_name="nsg1"
